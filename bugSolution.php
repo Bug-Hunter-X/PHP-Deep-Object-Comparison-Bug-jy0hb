8e9e00c7c8fb6aaf12070346c066ca434bec2d86
BugHunter-X <askzenai@gmail.com>
2025-01-01 02:52:06 +0100

    Add bugSolution.php file

---
+++ bugSolution.php
@@ -0,0 +1,25 @@
+function foo(a, b) {
+  if (a === b) {
+    return true;
+  }
+  if (typeof a !== 'object' || typeof b !== 'object' || a === null || b === null) {
+    return false;
+  }
+  const aKeys = Object.keys(a);
+  const bKeys = Object.keys(b);
+  if (aKeys.length !== bKeys.length) {
+    return false;
+  }
+  return aKeys.every(key => {
+    return b.hasOwnProperty(key) && deepCompare(a[key], b[key]);
+  });
+}
+
+function deepCompare(a, b) {
+  if (a === b) return true;
+  if (typeof a !== 'object' || typeof b !== 'object' || a === null || b === null) return false;
+  const aKeys = Object.keys(a);
+  const bKeys = Object.keys(b);
+  if (aKeys.length !== bKeys.length) return false;
+  return aKeys.every(key => deepCompare(a[key], b[key]));
+} 
